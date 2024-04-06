534f7d4b049d1738c688f2ffbeff360be558e6af
Restyled.io <commits@restyled.io>
2024-04-06 18:11:10 +0000

    Restyled by clang-format

--- src/show_information.c
+++ src/show_information.c
@@ -1,22 +1,21 @@
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
 
-void printLogScreen(pthread_mutex_t mutexLogFile, char logFile[100], char stringLog[600], char stringScreen[600])
-{
+void printLogScreen(pthread_mutex_t mutexLogFile, char logFile[100],
+                    char stringLog[600], char stringScreen[600]) {
 
-    FILE *log_file = fopen(logFile, "a"); // Archivo de log
+  FILE *log_file = fopen(logFile, "a"); // Archivo de log
 
-    if (log_file == NULL)
-    {
-        printf("Error al abrir el archivo de log.\n");
-        return;
-    }
+  if (log_file == NULL) {
+    printf("Error al abrir el archivo de log.\n");
+    return;
+  }
 
-    pthread_mutex_lock(&mutexLogFile); // Bloquear el mutex
+  pthread_mutex_lock(&mutexLogFile); // Bloquear el mutex
 
-    fprintf(log_file, "%s\n", stringLog); // Escribir en el archivo de log
-    printf("%s\n", stringScreen);         // Escribir en la pantalla
-    fclose(log_file);
+  fprintf(log_file, "%s\n", stringLog); // Escribir en el archivo de log
+  printf("%s\n", stringScreen);         // Escribir en la pantalla
+  fclose(log_file);
 
-    pthread_mutex_unlock(&mutexLogFile); // Desbloquear el mutex
+  pthread_mutex_unlock(&mutexLogFile); // Desbloquear el mutex
 }
